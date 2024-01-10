ef22c8733ea4a4565311a4ec1c78315ac8a03ec8
李闯 <chuang.li@ravsense.com>
2024-01-10 10:25:11 +0800

    malloc test

--- main/main.c
+++ main/main.c
@@ -30,11 +30,14 @@ enum
 
 static uint32_t led0_blink_interval_ms = BLINK_NOT_MOUNTED;
 static uint32_t led1_blink_interval_ms = BLINK_NOT_MOUNTED;
-
 #define URL "candas1.github.io/Hoverboard-Web-Serial-Control"
 
-#define DATA_SIZE 25600 // Total data size
-#define DATA_PAYLOAD DATA_SIZE
+/************************mallc 动态内存分配测试******************/
+#define TESTDATA 256
+#define TESTDATABUFFER 25600
+#define DATA_BUFFRE 2
+uint8_t *databuffer = NULL;
+/**************************************************************/
 
 const tusb_desc_webusb_url_t desc_url =
     {
@@ -51,11 +54,12 @@ typedef struct
 
 static volatile bool web_serial_connected = false;
 static volatile bool usb_connected = false;
-static volatile bool usb_send = true;
+static volatile bool usb_send = false;
 
 volatile uint64_t time_start1 = 0;
 volatile uint64_t time_1 = 0;
 
+void core1_entry();
 void led0_blinking_task(void);
 void led1_blinking_task(void);
 void web_printf(const char *format, ...);
@@ -71,15 +75,78 @@ int main()
 
     // init device stack on configured roothub port
     tud_init(BOARD_TUD_RHPORT);
-    // multicore_launch_core1(core1_entry);
+    multicore_launch_core1(core1_entry);
 
     gpio_put(LED0_PIN, 0);
+    databuffer = (uint8_t *)malloc(TESTDATABUFFER * DATA_BUFFRE);
+    if (databuffer == NULL)
+    {
+        uart_printf("malloc error\r\n");
+    }
+    for (uint32_t i = 0; i < TESTDATABUFFER; i++)
+    {
+        databuffer[i] = i % 0x100;
+    }
 
     while (1)
     {
+        tud_task(); // tinyusb device task
         led0_blinking_task();
+        // tud_cdc_write(databuffer, TESTDATABUFFER);
+        // sleep_ms(5);
+        usb_send = true;
+    }
+    free(databuffer);
+}
+
+void core1_entry()
+{
+    while (1)
+    {
+        if (usb_send && usb_connected && tud_cdc_write_available())
+        {
+            uint32_t cdc_write_available = tud_cdc_write_available();
+            // 判断一下testData的大小和tud_cdc_write_available()的大小
+            if (cdc_write_available < sizeof(TESTDATABUFFER))
+            {
+                uint32_t write_count = 0;
+                // 如果testData的大小小于tud_cdc_write_available()的大小，就只写入tud_cdc_write_available()的大小，分多次写入
+                while (1)
+                {
+                    tud_task(); // tinyusb device task
+                    led1_blinking_task();
+                    cdc_write_available = tud_cdc_write_available();
+                    if ((write_count + cdc_write_available) >= sizeof(TESTDATABUFFER))
+                    {
+                        tud_cdc_write(&((uint8_t *)&databuffer)[write_count], sizeof(TESTDATABUFFER) - write_count);
+                        write_count = sizeof(TESTDATABUFFER);
+                        break;
+                    }
+                    else
+                    {
+                        tud_cdc_write(&((uint8_t *)&databuffer)[write_count], cdc_write_available);
+                        write_count += cdc_write_available;
+                    }
+                }
+                usb_send = false;
+            }
+            else
+            {
+                tud_cdc_write((uint8_t *)&databuffer, sizeof(TESTDATABUFFER));
+                tud_task(); // tinyusb device task
+                led1_blinking_task();
+                usb_send = false;
+            }
+        }
+        else
+        {
+            sleep_ms(1);
+            tud_task(); // tinyusb device task
+            led1_blinking_task();
+        }
     }
 }
+
 
 void led0_blinking_task(void)
 {
